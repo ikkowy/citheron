c42b6151ae8d14db73e5381e01de7dac6c9250b0
Vincent Westenberg <vincent.westenberg@posteo.de>
2023-12-20 17:32:29 +0100

    Responsive side menu

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,8 +3,6 @@ import IconAccountCircle from "./components/icons/IconAccountCircle.vue";
 import IconMenu from "./components/icons/IconMenu.vue";
 import IconMenuOpen from "./components/icons/IconMenuOpen";
 import IconNotifications from "./components/icons/IconNotifications.vue";
-import IconPin from "./components/icons/IconPin.vue";
-import IconPinSlash from "./components/icons/IconPinSlash.vue";
 </script>
 
 <template>
@@ -14,11 +12,6 @@ import IconPinSlash from "./components/icons/IconPinSlash.vue";
     <a class="header-button" style="float: left" v-on:click="sideMenuOpen = !sideMenuOpen">
       <IconMenu v-if="!sideMenuOpen" />
       <IconMenuOpen v-else />
-    </a>
-
-    <a class="header-button" style="float: left" v-if="sideMenuOpen" v-on:click="sideMenuPinned = !sideMenuPinned">
-      <IconPin v-if="!sideMenuPinned" />
-      <IconPinSlash v-else />
     </a>
 
     <a class="header-button" style="float: right">
@@ -33,7 +26,7 @@ import IconPinSlash from "./components/icons/IconPinSlash.vue";
   <aside>
   </aside>
 
-  <main v-on:click="sideMenuOpen = false">
+  <main>
   </main>
 
 </template>
@@ -42,8 +35,35 @@ import IconPinSlash from "./components/icons/IconPinSlash.vue";
 import { ref, computed } from 'vue';
 
 const sideMenuOpen = ref(false);
-const sideMenuPinned = ref(false);
-const sideMenuWidth = ref("300px");
+
+const appWidth = ref(window.innerWidth);
+const appHeight = ref(window.innerHeight);
+
+window.addEventListener("resize", () => {
+  appWidth.value = window.innerWidth;
+  appHeight.value = window.innerHeight;
+});
+
+const sideMenuMaxWidth = 500;
+
+const sideMenuExpanded = computed(() => {
+  return sideMenuOpen.value && appWidth.value <= sideMenuMaxWidth;
+});
+
+const sideMenuWidthPx = computed(() => {
+  return sideMenuOpen.value ? `${sideMenuExpanded.value ? appWidth.value : sideMenuMaxWidth}px` : "0px";
+});
+
+const mainDisplay = computed(() => {
+  return sideMenuExpanded.value ? "none" : "inline-block";
+});
+
+const headerHeight = 40;
+const headerHeightPx = `${headerHeight}px`;
+
+const sideMenuHeightPx = computed(() => {
+  return `${appHeight.value - headerHeight}px`;
+});
 
 const sideMenuVisibility = computed(() => {
   return sideMenuOpen.value ? "visible" : "hidden";
@@ -61,7 +81,7 @@ body {
 
 header {
   width: 100%;
-  height: 40px;
+  height: v-bind(headerHeightPx);
   background-color: #1a1a1a;
 }
 
@@ -87,18 +107,19 @@ header {
 aside {
   box-sizing: border-box;
   visibility: v-bind('sideMenuVisibility');
-  width: v-bind('sideMenuWidth');
-  height: calc(100vh - 40px);
+  width: v-bind('sideMenuWidthPx');
+  height: v-bind('sideMenuHeightPx');
   background-color: #1a1a1a;
   border-top: 1px solid #262626;
 }
 
 main {
+  display: v-bind('mainDisplay');
   position: absolute;
+  overflow: scroll;
   top: 40px;
   right: 0;
-  width: calc(100vw - v-bind('sideMenuWidth'));
-  height: calc(100vh - 40px);
-  background-color: green;
+  width: calc(100vw - v-bind('sideMenuWidthPx'));
+  height: calc(100vh - v-bind('headerHeightPx'));
 }
 </style>
